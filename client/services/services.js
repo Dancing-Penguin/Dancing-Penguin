85fb6bd8c7e0766d6bea56fb82ead2f3f7a8c3e1
dan-h-ch <chang.daniel@gmail.com>
2016-09-05 22:02:30 -0700

    (feat) signin - confirmed correct format passed to userHandler on server

--- client/services/services.js
+++ client/services/services.js
@@ -10,7 +10,7 @@ angular.module("crowdcart.services",[])
       method: "POST",
       url: "/api/signin",
       // clarify on data format
-      data: user
+      data: JSON.stringify(user)
     })
     .then(function(res) {
       return res.data.token
