f67697a6654c754053967a3d41130ebdde078534
Kyle Van Vleck <kylevanvleck@gmail.com>
2016-09-05 22:10:59 -0500

    added getJobs method

--- server/users/userHandler.js
+++ server/users/userHandler.js
@@ -15,6 +15,30 @@ module.exports = {
     var email = req.body.email;
     var password = req.body.password;
 
+    User.findOne({'email': email}, function(err, user){
+      if (err) { // notifies if error is thrown
+        console.log("mongo findOne signin err: ", err);
+        helper.sendError(err, req, res);
+      } else {
+        if (!user) { // notifies if user is not found
+          helper.sendError("No user found", req, res);
+        } else {
+          user.comparePasswords(password, function(err, match){
+            if (!match) { // notifies if password is invalid
+              helper.sendError("Password invalid", req, res);
+            } else { // signin success, assigns jwt session token
+              var token = jwt.encode(user, 'secret');
+              res.json({
+                token: token, //session token will be set on client side
+                userid: user['_id']
+                // anything else to send back on success?
+              });
+            }
+          });
+        }
+      }
+    });
+
     // NOTE: This does a direct check for username and email.
     // TODO:
     // Setup password encryption in user model.
